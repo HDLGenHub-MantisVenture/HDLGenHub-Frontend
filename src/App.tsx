892841618b555d9b83467c182ecad7f15625c006
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2024-11-02 22:05:55 +0530

    up

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import './App.css'
 function App() {
   return (
     <>
-    <div className='flex items-center justify-center h-screen bg-green-100 text-4xl font-bold text-orange-600'>HDLGenHub is under developing</div>
+    <div className='flex items-center justify-center h-screen bg-green-100 text-4xl font-bold text-orange-600'>LoremIpsum is under developing</div>
     </>
   )
 }
 
 export default App
+
+
+//HDLGenHub
