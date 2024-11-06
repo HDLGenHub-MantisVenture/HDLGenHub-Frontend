8c78f9681f3ec4546c59fdc151c681d8bb8767ff
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2024-11-07 01:20:46 +0530

    up

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -10,19 +10,24 @@ const MainLayout: React.FC<MainLayoutProps> = ({navbar, children, sidebar}) =>{
     const [pageMinHeight, setPageMinHeight] = useState(0);
 
     useEffect(()=>{
-        if(navbarRef.current){
-            const height = navbarRef.current.offsetHeight;
-            setPageMinHeight(height);
+        const handleResize = () => {
+            if(navbarRef.current){
+                const height = navbarRef.current.offsetHeight;
+                setPageMinHeight(height);
+            }
+        }
+
+        handleResize();
+        window.addEventListener("resize", handleResize);
+        return ()=>{
+            window.removeEventListener("resize", handleResize);
         }
     },[]);
 
     return(
         <div className="flex flex-col w-full h-full">
             <div ref={navbarRef} className="">{navbar}</div>
-            <div className="flex flex-grow min-h-full"
-            style={{
-                height:  `calc(100vh - ${pageMinHeight}px)`
-            }}>
+            <div className={`min-h-[calc(100vh- ${pageMinHeight})] border-2 border-red-500`}>
                 <div className="h-full">{sidebar}</div>
                 <div className="w-full h-full">{children}</div>
             </div>
