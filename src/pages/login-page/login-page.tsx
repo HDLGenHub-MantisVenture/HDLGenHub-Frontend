ffa5d2a0d79d82a579cb48d3882c92b78d3b5152
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2024-11-09 16:52:23 +0530

    up

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -1,7 +1,16 @@
 const  LoginPage =()=>{
     return(
-            <div className="w-full h-full bg-red-100">
-                This is the login page
+            <div className="w-full h-full flex items-center justify-center px-8">
+                <div className="border-2 border-black max-w-[1200px] max-h-[800px] h-full w-full shadow-xl flex">
+                    <div className="w-1/2 border">Left</div>
+                    <div className="w-1/2 border">
+                        <div className="w-full h-full p-24 border border-red-400">
+                            <div className="border h-full">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
     )
 }
